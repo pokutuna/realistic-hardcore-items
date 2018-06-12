896eb241d295169d277a3974dca15aecd6999b03
pokutuna <popopopopokutuna@gmail.com>
2018-06-13 02:13:58 +0900

    loader

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const THREE = require('three');
 
-require('imports-loader?THREE=three!three/examples/js/loaders/MTLLoader');
-require('imports-loader?THREE=three!three/examples/js/loaders/OBJLoader');
+import 'expose-loader?THREE!three';
+import 'three/examples/js/loaders/MTLLoader';
+import 'three/examples/js/loaders/OBJLoader';
 
 console.log([THREE.OBJLoader, THREE.MTLLoader]);
